79df31c1738cabbfa077882943e3c2fa8248016e
ry <yeryanyin@gmail.com>
2026-01-28 09:23:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+def rectangle(y, z):
+    t.forward(y)
+    t.left(90)
+    t.forward(z)
+    t.left(90)
+    t.forward(y)
+    t.left(90)
+    t.forward(z)
+    t.left(90)
+rectangle(125, 100)
+
+t.right(180)
+
+def equal(x):
+    t.forward(x)
+    t.right(120)
+    t.forward(x)
+    t.right(120)
+    t.forward(x)
+    t.right(120)
+equal(90)
+
+turtle.done()
