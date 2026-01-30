000a2053361aeed37092510f118d77c1deaa5545
ry <yeryanyin@gmail.com>
2026-01-30 08:54:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ def rectangle(y, z):
     t.left(90)
 
 for i in range(60):
-    t.forward(i x 10)
+    print(i)
+    t.forward(i*2)
     t.left(90)
-    t.forward(i x 10)
+    t.forward(i*2)
     t.left(90)
-    t.forward(i x 10)
+    t.forward(i*2)
     t.left(90)
-    t.forward(i x 10)
+    t.forward(i*2)
     t.left(95)
 
 turtle.done()
